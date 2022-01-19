e5bd867f8d1506aedf28ec0923c38cbf7eb17f76
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-19 09:43:18 -0300

    c48- adding modify css

--- practice.htmlandcss/anime.js
+++ practice.htmlandcss/anime.js
@@ -1,22 +1,24 @@
 
 let jogador  = document.getElementById('jogador')
 
-jogador.style.top ='40px'
-jogador.style.left ='50px'
-jogador.style.right = '50px'
-
+jogador.style.top ='30px'
+jogador.style.left ='60px'
+jogador.style.right = '70px'
 
 let Yinitial = 0
 let Xinitial = 0
 let Zinitial = 0
 
-
 const Moveon = (x,y,z) =>{
-   
   let posy = y +'py'
   let posx = x +'px'
   let posz = z + 'pz'
- jogador.style.top = posy
- jogador.style.left =posx
+ jogador.style.top = posx
+ jogador.style.left =posy
  jogador.style.right = posz
 }
+
+
+setInterval(() => {
+ Moveon(Yinitial++, Xinitial++, Zinitial++)    
+}, 100);
